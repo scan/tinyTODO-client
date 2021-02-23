5396a8def3d4910f4dd21da0fb3347f542b97a21
Tom Streller <tom.streller@tutti.ch>
2021-02-23 07:32:31 +0100

    setup

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import theme from "~/theme";
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+App.displayName = "App";
+
+export default App;
