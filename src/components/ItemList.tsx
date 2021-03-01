d59ecf59612e778b4ee3d4ea770b9b5336ef0ccf
Tom Streller <tom.streller@tutti.ch>
2021-03-01 14:33:45 +0100

    Add load more button

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
-import { graphql, createPaginationContainer, RelayPaginationProp } from "react-relay";
+import {
+  graphql,
+  createPaginationContainer,
+  RelayPaginationProp,
+} from "react-relay";
 
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 import type { ItemList_list } from "./__generated__/ItemList_list.graphql";
 
@@ -14,18 +19,43 @@ interface Props {
 }
 
 const ItemList: React.FC<Props> = ({ list, relay }) => {
+  // Otherwise the re-render does not work when `isLoading` changes
+  const isLoading = React.useMemo(() => relay.isLoading(), [relay]);
+
+  const handleLoadMoreClick = React.useCallback(
+    (event: React.MouseEvent) => {
+      event.preventDefault();
+
+      relay.loadMore(5);
+    },
+    [relay]
+  );
+
   const handleCreationSuccess = React.useCallback(() => {
     relay.refetchConnection(5);
-  }, [relay])
+  }, [relay]);
 
   return (
     <>
-      <Grid container spacing={2}>
-        {list.items.edges.map((edge) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={edge.node.id}>
-            <Item item={edge.node} />
+      <Grid container direction="column">
+        <Grid item container spacing={2}>
+          {list.items.edges.map((edge) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={edge.node.id}>
+              <Item item={edge.node} />
+            </Grid>
+          ))}
+        </Grid>
+        {relay.hasMore() && (
+          <Grid item>
+            <Button
+              fullWidth
+              disabled={isLoading}
+              onClick={handleLoadMoreClick}
+            >
+              Load More
+            </Button>
           </Grid>
-        ))}
+        )}
       </Grid>
       <CreateItemButton onSuccess={handleCreationSuccess} />
     </>
